cb9ef2da8bc2d9181e3a51fa6457bb1bcc480c06
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/ImporterManager.php
+++ src/Managers/ImporterManager.php
@@ -12,6 +12,14 @@ use Illuminate\Support\Collection;
 use Railken\Lem\Manager;
 use Railken\Lem\Result;
 
+/**
+ * @method \Amethyst\Models\Importer newEntity()
+ * @method \Amethyst\Schemas\ImporterSchema getSchema()
+ * @method \Amethyst\Repositories\ImporterRepository getRepository()
+ * @method \Amethyst\Serializers\ImporterSerializer getSerializer()
+ * @method \Amethyst\Validators\ImporterValidator getValidator()
+ * @method \Amethyst\Authorizers\ImporterAuthorizer getAuthorizer()
+ */
 class ImporterManager extends Manager
 {
     use ConfigurableManager;
