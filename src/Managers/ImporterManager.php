e3ded71d6c233867091daa28ab75f5aa72094241
railken <railken@protonmail.ch>
2020-01-19 18:28:49 +0100

    fix namespaces

--- src/Managers/ImporterManager.php
+++ src/Managers/ImporterManager.php
@@ -2,7 +2,7 @@
 
 namespace Amethyst\Managers;
 
-use Amethyst\Common\ConfigurableManager;
+use Amethyst\Core\ConfigurableManager;
 use Amethyst\Jobs\ImportCsvFile;
 use Amethyst\Jobs\ImportJsonFile;
 use Amethyst\Jobs\ImportXlsxFile;
